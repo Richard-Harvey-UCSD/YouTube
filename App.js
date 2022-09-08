c22e54fe7b1ce63b28a0f21b22d401a992021a86
Richard Harvey <theerichardharvey@gmail.com>
2022-09-08 16:37:50 -0400

    ESLint example

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,12 +6,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  // eslint-disable-next-line react-native/no-color-literals
   container: {
     flex: 1,
     backgroundColor: '#fff',
