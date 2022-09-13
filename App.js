69302e3f98f7971b3d96fdc8c3236b053f81262f
Richard Harvey <theerichardharvey@gmail.com>
2022-09-13 00:23:06 -0400

    Added useMemo hook

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import UseReducerHook from './src/UseReducerHook';
+import UseMemoHook from './src/UseMemoHook';
 
 export default function App() {
   return (
     <View style={styles.view}>
-      <Text style={{ fontSize: 40, marginBottom: 10 }}>useReducer</Text>
-      <Text style={{ fontSize: 40, marginBottom: 50 }}>in React Native</Text>
-      <UseReducerHook />
+      <Text style={{ fontSize: 40, marginBottom: 10 }}>useMemo</Text>
+      <Text style={{ fontSize: 40, marginBottom: 10 }}>in React Native</Text>
+      <UseMemoHook />
     </View >
   );
 }
---
+++ src/UseMemoHook.js
@@ -0,0 +1,49 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useMemo, useState } from 'react';
+
+const useMemoHook = () => {
+  const [count, setCount] = useState(0);
+  const [memoizedCount, setMemoizedCount] = useState(0);
+
+  const increment = () => {
+    setCount(prevState => prevState + 1);
+  };
+
+  const memoizedIncrement = () => {
+    setMemoizedCount(prevState => prevState + 1);
+  };
+
+  const expensiveOperation = num => {
+    for (let i = 0; i < 1000000000; i++) {
+      num += 1;
+    }
+    return num;
+  };
+
+  // const operation = expensiveOperation(count);
+  const operation = useMemo(() => expensiveOperation(count), [count])
+
+  console.log('Count:', count);
+  console.log(`Memoized Count: ${memoizedCount} \n`)
+
+  return (
+    <>
+      <View style={{ marginTop: 50, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 30 }}>Count: {count}</Text>
+        <TouchableOpacity onPress={increment}>
+          <Text style={{ fontSize: 30 }}>+</Text>
+        </TouchableOpacity>
+        <Text>Expensive Operation: {operation}</Text>
+      </View>
+
+      <View style={{ marginTop: 50, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontSize: 30 }}>Memoized Count: {memoizedCount}</Text>
+        <TouchableOpacity onPress={memoizedIncrement}>
+          <Text style={{ fontSize: 30 }}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+export default useMemoHook;
