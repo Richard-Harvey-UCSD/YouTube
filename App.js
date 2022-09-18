32255e1e8a8e2304570359d3942f003a5cdb8432
Richard Harvey <theerichardharvey@gmail.com>
2022-09-18 14:47:08 -0400

    Setup useImperativeHandle hook example

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import UseLayoutEffectHook from './src/UseLayoutEffectHook';
+import UseImperativeHandleHook from './src/UseImperativeHandleHook';
 
 export default function App() {
   return (
     <View style={styles.view}>
       <Text style={{ fontSize: 40, marginBottom: 10 }}>useLayoutEffect</Text>
       <Text style={{ fontSize: 40, marginBottom: 10 }}>in React Native</Text>
-      <UseLayoutEffectHook />
+      <UseImperativeHandleHook />
     </View>
   );
 }
---
+++ src/UseImperativeHandleHook.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const UseImperativeHandleHook = () => {
+  return (
+    <View>
+      <Text>UseImperativeHandleHook</Text>
+    </View>
+  );
+};
+
+export default UseImperativeHandleHook;
